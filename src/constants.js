bc6d8501f30d173a55f79da0d7762d65409d2853
shashankdhar75 <shashankdhar75@gmail.com>
2020-04-08 23:16:18 -0400

    API Key update

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 export const URL_WEATHER = `http://api.openweathermap.org/data/2.5/weather?zip=`;
 export const WEATHER_API_KEY = `68164a451833b86253068b25ae03d285`;
 export const URL_PLACES = `https://api.yelp.com/v3/businesses/search?location=NY `;
-export const PLACES_API_KEY = `tcUtoLFop1Soic0c7bchNOiQHRBQRGYxl4FZUzxLlWsiv3eVrKdmsVLBYnCDsUocTZQ_w9elFfSvirdt36Rt9BquQF9tU6zlDeHFPcUzyu6d6lf-0BxAW-DRBsqjXHYx`;
+export const PLACES_API_KEY = `jeN6-CUUJGLWRIlBSi5wYbGg2iGJqLbs9RYt8Q1Q8iA-7tB_lq-L0vo-oSSBHuHMh7Fai5jTN1EWN7hkj4xwUkgRNbEdvnZWd4jGfsgBO1NlRdb0x3T9J6OB8oYFXXYx`;
 
 export const URL_TRAFFIC = `http://dev.virtualearth.net/REST/v1/Traffic/Incidents/`;
 export const TRAFFIC_API_KEY = `Atn_u4NLEb_kYQvm0FSqNOLrqp-AQoEpSsVGtqtUk3AQQjKHfzV5uZhYABzbqRAV`;
@@ -10,9 +10,15 @@ export const URL_CRIME = `https://data.cityofnewyork.us/resource/7x9x-zpz6.json?
 export const CRIME_API_KEY = `ntKrW4IM7tt2bdmm6uNBShREI`;
 
 export const CATEGORIES_PLACES = [
-  { label: "Food", value: `restaurants,coffee,fooddeliveryservices,foodtrucks,grocery,markets` },
-  { label: "Health", value: `hospitals,dentists,physicians,medcenters` },
-  { label: "Public Services", value: `policedepartments,firedepartments,embassy,landmarks,municipality,postoffices,taxoffice,townhall` },
-  { label: "Hotels", value: `hotels,guesthouses,healthretreats` },
-  { label: "Education", value: `education` }
+  {
+    label: 'Food',
+    value: `restaurants,coffee,fooddeliveryservices,foodtrucks,grocery,markets`
+  },
+  { label: 'Health', value: `hospitals,dentists,physicians,medcenters` },
+  {
+    label: 'Public Services',
+    value: `policedepartments,firedepartments,embassy,landmarks,municipality,postoffices,taxoffice,townhall`
+  },
+  { label: 'Hotels', value: `hotels,guesthouses,healthretreats` },
+  { label: 'Education', value: `education` }
 ];
