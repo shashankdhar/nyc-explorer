c83bdfac1d7308567d8a461ae7a33dc017bfe589
shashankdhar75 <shashankdhar75@gmail.com>
2020-04-08 23:17:54 -0400

    Removed debug messages

--- src/components/MapPath.js
+++ src/components/MapPath.js
@@ -1,49 +1,47 @@
-import React from "react";
-import { Redirect } from "react-router-dom";
+import React from 'react';
+import { Redirect } from 'react-router-dom';
 
 export default class MapPath extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			zip: this.props.data.properties.ZIPCODE,
-			county: this.props.data.properties.COUNTY,
-			coordinates: this.props.data.geometry.coordinates[0],
-			redirect: false
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      zip: this.props.data.properties.ZIPCODE,
+      county: this.props.data.properties.COUNTY,
+      coordinates: this.props.data.geometry.coordinates[0],
+      redirect: false
+    };
+  }
 
-	handleZipClick = () => {
-		this.setState({ redirect: true });
-	};
+  handleZipClick = () => {
+    this.setState({ redirect: true });
+  };
 
-	render( history ) {
-		const { redirect } = this.state;
-		console.log(history);
-		debugger;
-		if (redirect) {
-			return (
-				<Redirect
-					to={{
-						pathname: "/zip",
-						state: {
-							zip: this.state.zip,
-							county: this.state.county,
-							population: this.props.data.properties.POPULATION,
-							coordinates: this.props.data.geometry.coordinates[0]
-						}
-					}}
-				/>
-			);
-		}
+  render(history) {
+    const { redirect } = this.state;
+    if (redirect) {
+      return (
+        <Redirect
+          to={{
+            pathname: '/zip',
+            state: {
+              zip: this.state.zip,
+              county: this.state.county,
+              population: this.props.data.properties.POPULATION,
+              coordinates: this.props.data.geometry.coordinates[0]
+            }
+          }}
+        />
+      );
+    }
 
-		return (
-			<path
-				d={this.props.path}
-				fill={this.props.color}
-				onClick={this.handleZipClick}
-			>
-				<title>Zipcode : {this.props.data.properties.ZIPCODE}</title>
-			</path>
-		);
-	}
+    return (
+      <path
+        d={this.props.path}
+        fill={this.props.color}
+        onClick={this.handleZipClick}
+      >
+        <title>Zipcode : {this.props.data.properties.ZIPCODE}</title>
+      </path>
+    );
+  }
 }
